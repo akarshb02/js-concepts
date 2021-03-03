0b85cc79d23815349ff6e104fc74b29c441e6715
Akarsh <akarshb02@gmail.com>
2021-03-03 19:43:13 +0530

    Higher order function

---
+++ calculate.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+     return num1 + num2;
+
+ }
+ function sub(num1, num2) {
+     return num1 - num2;
+ }
+
+function calculte(num1, num2, operator) {
+return operator(num1, num2);
+ }
+
+
+ calculte(1, 2, add);
+
+
+//output
+//3
