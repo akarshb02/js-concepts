a017558fbe5bada434fde8a1c8abaf0c0b4dd323
Akarsh <akarshb02@gmail.com>
2021-03-08 19:52:40 +0530

    example (var,let and const)

---
+++ var_let_const.js
@@ -0,0 +1,17 @@
+var num = 2
+const value = 5;
+
+function sum() {
+
+    let i = 3;
+    var num = 4;
+    let i = 6;
+
+    for (i = 0; i <= 1; i++) {
+
+        let res = num + value;
+        console.log(res);
+
+    }
+}
+sum();
