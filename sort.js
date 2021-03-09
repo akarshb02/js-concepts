6596d28dd7c8dbab9de2b14ad06e6043ad206f08
Akarsh <akarshb02@gmail.com>
2021-03-09 16:58:50 +0530

    updated with array of objs

--- sort.js
+++ sort.js
@@ -1,5 +1,15 @@
 // sort all the records in ascending order of their age
 
+
+var testArray = [{ 'name': 'jai', 'age': 26 },
+    { 'name': 'shiv', 'age': 22 },
+    { 'name': 'Anuj', 'age': 5 },
+    { 'name': 'Tom', 'age': 20 },
+    { 'name': 'Jerry', 'age': 20 }
+];
+
+
+
 const ageSort = testArray.sort((a, b) => {
       
         return a.age - b.age;
